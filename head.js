e3f204850f96c4990cbd9c536c8361605d11e0cc
Joel Hagan <joel.hagan.8@gmail.com>
2022-11-18 14:06:47 -0700

    added a head function that allows for the first item of an array to be pulled

---
+++ head.js
@@ -0,0 +1,20 @@
+const head = function(array) {
+  if (array === []) {
+    return undefined
+  } else {
+  return array[0]
+  }
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    return (`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  if (actual === expected) {
+    return (`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+console.log(assertEqual(head([]), 2));
+console.log(assertEqual(head([5,6,7]), 5));
+console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
