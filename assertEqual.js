716a34662d7136f7462abfca05d6cd1381bb0589
Joel Hagan <joel.hagan.8@gmail.com>
2022-11-17 20:31:33 -0700

    made a function to compare different arguments whether a number or string

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    return ("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+  if (actual === expected) {
+    return ("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  }
+};
+
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
