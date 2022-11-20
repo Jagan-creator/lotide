505d8e56022c79fb57d9273371d80146a9cea5c9
Joel Hagan <joel.hagan.8@gmail.com>
2022-11-20 12:29:54 -0700

    made a function that takes in two arrays and returns a new array that does not involve any items from the second input array

---
+++ without.js
@@ -0,0 +1,32 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i])
+      return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(firstArray, secondArray) {
+  if (eqArrays(firstArray, secondArray)) {
+    return (`✅✅✅ Assertion Passed: ${firstArray} === ${secondArray}`);
+  } else {
+    return (`🛑🛑🛑 Assertion Failed: ${firstArray} !== ${secondArray}`);
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  for (let i = 0; i < source.length; i++) {
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] === itemsToRemove[j]) {
+        source.splice(i, 1);
+      }
+    }
+  }
+  return source;
+};
+
+console.log(without([1, 2, 3], [1, 2]));
+console.log(without(["hi", "yes", "test"], ["hi", "test"]));
