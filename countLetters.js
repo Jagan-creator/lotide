09bcf22c180252c5f3e89503d4006859c48258ae
Joel Hagan <joel.hagan.8@gmail.com>
2022-11-24 19:16:27 -0700

    created the core of the countLetters function that will be ran

--- countLetters.js
+++ countLetters.js
@@ -10,7 +10,17 @@ const assertEqual = function(actual, expected) {
 
 // COUNT LETTERS FUNCTION
 const countLetters = function(sentenceString) {
-
+  let results = {};
+  for (let letter of sentenceString) {
+    if (letter !== " ") {       // avoids dealing with spaces in the strings we run
+      if (results[letter]) {
+        results[letter] += 1;   // this adds 1 to all letters of that type that already have been run
+      } else {
+        results[letter] = 1;    // this creates a 1 for letter that have not yet been discovered
+      }
+    }
+  }
+  return results;
 };
 
 // TEST PARAMATERS
