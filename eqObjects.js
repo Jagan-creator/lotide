57274fe7a055c64a18db11a2d356d9d930b9f0e0
Joel Hagan <joel.hagan.8@gmail.com>
2022-11-25 14:23:30 -0700

    made some variables that can be used throughout our function, may need to add onto this list as I work further through the function

--- eqObjects.js
+++ eqObjects.js
@@ -11,6 +11,16 @@ const assertEqual = function(actual, expected) {
 // function below is supposed to take in two objects and return a true value if they match and a false value if they do not
 
 const eqObjects = function(object1, object2) {
+  // set out some variables at the start of function to help simplify the code and make things easier to read/write
+  let equalObjects = false;                   // this will help us compare outputs later in the function
+  let insert1 = Object.keys(object1);         // easier to write out the Object.keys method here
+  let insert2 = Object.keys(object2);         // same as above but just for object2 now
+
+  // set an if parameter that indicates if the key length on both objects does not match it will return a false value
+
+
+  // now setup a for if scenario that involves using isArray so we can compare them to return an appropriate value
+
 
 };
 
