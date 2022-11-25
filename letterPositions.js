150e67fca0c7467a8cb5e9765393b393b756a1ea
Joel Hagan <joel.hagan.8@gmail.com>
2022-11-24 19:31:04 -0700

    made the base foundation for a letterPositions function that will tell us the index location of letters

---
+++ letterPositions.js
@@ -0,0 +1,7 @@
+// LETTER POSITION FUNCTION
+const letterPositions = function(sentence) {
+  const results = {};
+  // steps here for inputing the proper data for results
+};
+
+// TESTING PARAMATERS
