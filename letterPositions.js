a6e89094636014e3fba2d71ade0f5e6362a03666
Joel Hagan <joel.hagan.8@gmail.com>
2022-11-24 19:38:14 -0700

    added in eqArrays function and assertArraysEqual function

--- letterPositions.js
+++ letterPositions.js
@@ -1,3 +1,24 @@
+// ARRAY PASS EQUAL FUNCTION
+const assertArraysEqual = function(firstArray, secondArray) {
+  if (eqArrays(firstArray, secondArray)) {
+    return (`✅✅✅ Assertion Passed: ${firstArray} === ${secondArray}`);
+  } else {
+    return (`🛑🛑🛑 Assertion Failed: ${firstArray} !== ${secondArray}`);
+  }
+};
+
+// ARRAY EQUAL FUNCTION
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i])
+      return false;
+  }
+  return true;
+};
+
 // LETTER POSITION FUNCTION
 const letterPositions = function(sentence) {
   const results = {};
