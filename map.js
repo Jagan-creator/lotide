bdda7dcd623e728b95ce6f9d41c148a39918783d
Joel Hagan <joel.hagan.8@gmail.com>
2022-11-27 15:15:47 -0700

    added in assertArraysEqual function and eqArrays function to help further test our results of the newly developed map function

--- map.js
+++ map.js
@@ -1,14 +1,37 @@
+// ARRAY EQUALS
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i])
+      return false;
+  }
+  return true;
+};
+
+// ASSERTION FOR ARRAY EQUALS
+const assertArraysEqual = function(firstArray, secondArray) {
+  if (eqArrays(firstArray, secondArray)) {
+    return (`✅✅✅ Assertion Passed: ${firstArray} === ${secondArray}`);
+  } else {
+    return (`🛑🛑🛑 Assertion Failed: ${firstArray} !== ${secondArray}`);
+  }
+};
+
+
+// NEW MAP FUNCTION FOR ARRAYS
 const words = ["this", "test", "is", "for", "implementing", "maps"];
 
 const map = function(array, callback) {
-  // temp code
-  console.log("array ", array);
-  console.log("callback ", callback);
-  
   const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
   return results;
 };
 
 const results1 = map(words, word => word[0]);
 
+//TESTING CONDITIONS
 console.log(results1);
