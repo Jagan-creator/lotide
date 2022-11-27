9313692f2ad9426987cc2decbb69c7c2515c7978
Joel Hagan <joel.hagan.8@gmail.com>
2022-11-27 15:00:05 -0700

    added in some temp code that outputs our array argument and our callback argument

--- map.js
+++ map.js
@@ -1,7 +1,12 @@
 const words = ["this", "test", "is", "for", "implementing", "maps"];
 
-const map = function() {
-  // empty
+const map = function(array, callback) {
+  // temp code
+  console.log("array ", array);
+  console.log("callback ", callback);
+  
+  const results = [];
+  return results;
 };
 
 const results1 = map(words, word => word[0]);
