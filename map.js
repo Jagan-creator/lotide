e2d434ebc348e0a1d8a817fd41f651ecbdccf037
Joel Hagan <joel.hagan.8@gmail.com>
2022-11-27 14:55:59 -0700

    added in a blank function for map

--- map.js
+++ map.js
@@ -1,5 +1,9 @@
 const words = ["this", "test", "is", "for", "implementing", "maps"];
 
+const map = function() {
+  // empty
+};
+
 const results1 = map(words, word => word[0]);
 
 console.log(results1);
