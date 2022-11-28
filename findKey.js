30ca026ed403357cddb40ac4595c0a1bf247267c
Joel Hagan <joel.hagan.8@gmail.com>
2022-11-27 19:36:51 -0700

    set up baseline for assertion function and our new findKey function that will intake an object and a callback argument

---
+++ findKey.js
@@ -0,0 +1,24 @@
+// ASSERTION EQUAL FUNCTION
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    return (`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  if (actual === expected) {
+    return (`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+// FIND KEY FUNCTION
+const findKey = function(object, callback) {
+  // empty for now
+};
+
+// TEST CONDITIONS
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2));
