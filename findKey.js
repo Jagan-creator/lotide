dbb3d71b1037622b9a70ad46d58e6884876fee13
Joel Hagan <joel.hagan.8@gmail.com>
2022-11-27 19:44:47 -0700

    findKey function is fully implemented and returns the object item when it has reached our given callback argument

--- findKey.js
+++ findKey.js
@@ -10,15 +10,19 @@ const assertEqual = function(actual, expected) {
 
 // FIND KEY FUNCTION
 const findKey = function(object, callback) {
-  // empty for now
+  for (let item in object) {
+    if (callback(object[item])) {
+      return item;
+    }
+  }
 };
 
 // TEST CONDITIONS
-console.log(findKey({
+console.log(assertEqual(findKey({
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2));
+}, x => x.stars === 2), "noma"));
