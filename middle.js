442536ce0c07e58eede5fdc0a111c7782b304b6e
Joel Hagan <joel.hagan.8@gmail.com>
2022-12-07 16:49:26 -0700

    refactored the testing conditions to be put in exported files

--- middle.js
+++ middle.js
@@ -1,21 +1,5 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i])
-      return false;
-  }
-  return true;
-};
-
-const assertArraysEqual = function(firstArray, secondArray) {
-  if (eqArrays(firstArray, secondArray)) {
-    return (`✅✅✅ Assertion Passed: ${firstArray} === ${secondArray}`);
-  } else {
-    return (`🛑🛑🛑 Assertion Failed: ${firstArray} !== ${secondArray}`);
-  }
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 const middle = function(arr) {
   if (arr.length <= 2) {
@@ -30,6 +14,8 @@ const middle = function(arr) {
   }
 };
 
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
-console.log(middle([6]));
-console.log(middle([2, 4]));
+// console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+// console.log(middle([6]));
+// console.log(middle([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]));
+
+module.exports = middle;
