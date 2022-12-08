bacde8d335d575d1cb2df5a7de0f1a9aaa18cfd3
Joel Hagan <joel.hagan.8@gmail.com>
2022-12-07 18:10:50 -0700

    added in mocha and chai testing functionality to the tail function

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,33 @@
-// EXPORT
+// // EXPORT
 
-const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+// const tail = require('../tail');
+// const assertEqual = require('../assertEqual');
 
-// TEST CODE
+// // TEST CODE
+
+// let result = tail(["Lighthouse", "Labs", "Hello", "Test"]);
+
+// console.log(assertEqual(result[1], "Hello"));
+// console.log(assertEqual(result[2], "Test"));
+// console.log(assertEqual(result[0], "Labs"));
+// console.log(assertEqual(result.length, 3));
+
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
 let result = tail(["Lighthouse", "Labs", "Hello", "Test"]);
 
-console.log(assertEqual(result[1], "Hello"));
-console.log(assertEqual(result[2], "Test"));
-console.log(assertEqual(result[0], "Labs"));
-console.log(assertEqual(result.length, 3));
+describe("#head", () => {
+  it(`returns "Hello" for ["Lighthouse", "Labs", "Hello", "Test"]`, () => {
+    assert.strictEqual(result[1], "Hello");
+  });
+  it(`returns "Test" for ["Lighthouse", "Labs", "Hello", "Test"]`, () => {
+    assert.strictEqual(result[2], "Test");
+  });
+  it(`returns "Labs" for ["Lighthouse", "Labs", "Hello", "Test"]`, () => {
+    assert.strictEqual(result[0], "Labs");
+  });
+  it(`returns 3 for ["Lighthouse", "Labs", "Hello", "Test"]`, () => {
+    assert.strictEqual(result.length, 3);
+  });
+});
